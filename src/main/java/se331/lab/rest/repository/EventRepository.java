f44d9fb4b8323b97e18919ec18a77ccfe62c2605
Decha_L <blackneko1234@hotmail.com>
2021-09-15 20:05:34 +0700

    1.6
    1.6

--- src/main/java/se331/lab/rest/repository/EventRepository.java
+++ src/main/java/se331/lab/rest/repository/EventRepository.java
@@ -9,10 +9,8 @@ import se331.lab.rest.entity.Event;
 
 import java.util.List;
 
-public interface EventRepository extends JpaRepository<Event,Long> {
+public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findAll();
 
-    Page<Event> findByTitle(String title, Pageable pageRequest);
-
-    Page<Event> findByTitleContaining(String title, Pageable pageRequest);
+    Page<Event> findByTitleContainingOrDescriptionContaining(String title, String description, Pageable pageRequest);
 }
